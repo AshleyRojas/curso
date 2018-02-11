2ff88c35a481e2d6f292b0fd5b7ad55f0f997629
ashleyrojas <aalbertorv@gmail.com>
2018-02-11 16:30:28 -0600

    clase del domingo

--- index.php
+++ index.php
@@ -1,8 +1,37 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Laboratorio2
- * Date: 11/02/2018
- * Time: 14:58
- */
-echo "hi";
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mi pagina</title>
+</head>
+<body>
+
+<header style="background-color: black; width: 100%; height: 200px; ">
+    <section style="background-color: blue;  width: 100%; height: 50px;">
+        <p>soy un parrafo</p>
+    </section>
+    <nav style="background-color: orange;  width: 100%; height: 80px;">
+        <a href="#">Ir a inicio</a>
+    </nav>
+</header>
+
+<section>
+    <h1>hola soy la seccion 1</h1>
+</section>
+<section>
+    <h1>hola soy la seccion 2</h1>
+</section>
+<section>
+    <h1>hola soy la seccion 3</h1>
+</section>
+<section>
+    <h1>hola soy la seccion 4</h1>
+</section>
+<section>
+    <h1>hola soy la seccion 5</h1>
+</section>
+</body>
+</html>
